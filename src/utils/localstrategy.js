5bca5820abf7983848cf82f300b3f4cdcca7f95b
uwuxverticuse <sagargusain150@gmail.com>
2024-04-01 21:09:55 +0530

    LocalStrategy

--- src/utils/localstrategy.js
+++ src/utils/localstrategy.js
@@ -1,9 +1,24 @@
 import passport from 'passport'
 import {Strategy as localStrategy} from 'passport-local'
-
+import User from '../models/user.models.js'
 passport.use(new localStrategy(
-    async(username,password,done)=>{
+    async(userName,password,done)=>{
+        try {
+            const user = await User.findOne({username:userName})
+            if(!user){
+                return done(null,false,{message:'User not found'})
+            }
+            if(!user.matchPassword(password)){
+                return done(null,false,{message:'Invalid Password'})
+            }
+            return done(null,user)
+        } catch (error) {
+            console.log(`Error Signing up User: ${error.message}`);
+            
+        }
+
         
+
 
     }
 ))

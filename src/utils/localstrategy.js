7c8215ca1f344df09e32860d12f8b14d307db0d0
uwuxverticuse <sagargusain150@gmail.com>
2024-04-03 04:54:02 +0530

    Strategy Part

--- src/utils/localstrategy.js
+++ src/utils/localstrategy.js
@@ -1,25 +1,18 @@
-import passport from 'passport'
-import {Strategy as localStrategy} from 'passport-local'
-import User from '../models/user.models.js'
-passport.use(new localStrategy(
-    async(userName,password,done)=>{
-        try {
-            const user = await User.findOne({username:userName})
-            const matchPassword = await user.matchPassword({password:password})
+import passport from "passport";
+import { Strategy as localStrategy } from "passport-local";
+import User from "../models/user.models.js";
+passport.use(
+  new localStrategy(async (userName, password, done) => {
+    try {
+      const user = await User.findOne({ username: userName });
+      const matchPassword = await user.matchPassword({ password: password });
 
-            if(!user || !matchPassword){
-                return done(null,false,{message:'Invalid Username or Password'})
-            }
-            return done(null,user)
-
-    
-        } catch (error) {
-            return done(error,false,{message:'Error Signing up User'})
-            
-        }
-
-        
-
-
+      if (!user || !matchPassword) {
+        return done(null, false, { message: "Invalid Username or Password" });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error, false, { message: "Error Signing up User" });
     }
-))
+  })
+);

5468bee25cdb51729262c49523d03fddfe793820
uwuxverticuse <sagargusain150@gmail.com>
2024-04-03 20:31:56 +0530

    person Routes to get  the Person data

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import passport from "../utils/localstrategy.js";
 import signup from '../controllers/signup.controllers.js'
+import person from '../controllers/person.controllers.js'
 const router = express.Router();
 
 router.post('/signup',signup)
-
+router.get('/person',passport.authenticate('local', { session: false }),person)
 export default router;

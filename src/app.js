fcf9eaba3682e5e62d50732e11c1c346be150ff2
uwuxverticuse <sagargusain150@gmail.com>
2024-04-03 20:32:47 +0530

    Intializing the Passport

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
-
+import passport from "./utils/localstrategy.js";
 const app = express(); 
-
+app.use(passport.initialize());
 
 app.use(express.json({limit:'16kb',extended :true}))
 
